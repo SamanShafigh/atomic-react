6d5511db6266d063a297cc2784c9b006fa5b1079
Saman Shafigh <samanshafigh@gmail.com>
2018-02-20 11:14:03 +1100

    update readme

--- src/share/organisms/Post/Post.story.js
+++ src/share/organisms/Post/Post.story.js
@@ -17,5 +17,22 @@ story.add('default', () => (
 story.add('posting post', () => (
   <div>
     <Post post={newPost}  />
+
+    <pre>{`
+        const post = {
+          id: 1,
+          summary: "Excited to be in Melbourne mentoring smart folks from Indonesia & Australia...",
+          img: "",
+          likes: 12,
+          user: {
+            id: 'sina',
+            name: 'Sina',
+            email: 'sina@gmail.com',
+            img: 'static/profile/g.jpg'
+          },
+        };
+        
+        return <Post post={post}  />
+    `}</pre>
   </div>
 ));
